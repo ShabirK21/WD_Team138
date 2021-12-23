c6417eacfc14778282495b5bff537739899a83b6
rajeevraul <rajeev.raul99@gmail.com>
2021-12-24 01:21:58 +0800

    alert edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function newsletter() {
   if (document.getElementById("email").value == "") {
-    alert("Please input a valud email address");
+    alert("Please input a valid email address");
   } else {
     alert("Thank you for subscribing to our newsletter!");
   }
