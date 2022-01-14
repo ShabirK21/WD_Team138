dd864b11d43a54282c0b1875332f3f5becaaed6a
ShabirK21 <shaber213@gmail.com>
2022-01-14 23:33:20 +0800

    minor cleanup

--- tickets.js
+++ tickets.js
@@ -1,6 +1,6 @@
 function total_adult() {
   var no_of_tickets = document.getElementById("adultTic").value;
-  total = no_of_tickets * 30;
+  var total = no_of_tickets * 30;
   document.getElementById("adult_total").innerHTML = "$" + total;
   return total;
 }
@@ -14,23 +14,21 @@ function total_youth() {
 
 function total_children() {
   var no_of_tickets = document.getElementById("childTic").value;
-  total = no_of_tickets * 10;
+  var total = no_of_tickets * 10;
   document.getElementById("child_total").innerHTML = "$" + total;
   return total;
 }
 
 function all_total() {
-  total = total_adult() + total_youth() + total_children();
+  var total = total_adult() + total_youth() + total_children();
   document.getElementById("all_total").innerHTML = "$" + total;
 }
 
 function booking() {
-  var dom=document.getElementById("paymentmethod");
-  if(dom.options[dom.selectedIndex].value=="null")
-  {
+  var dom = document.getElementById("paymentmethod");
+  if (dom.options[dom.selectedIndex].value == "null") {
     alert("Please select your tickets");
-  }
-  else{
+  } else {
     alert("Thank you for booking with us");
   }
 }
