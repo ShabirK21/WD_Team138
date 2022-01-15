ea46c16be580b09eb02e93b6037408679f67fc05
Rahil8533 <sukhoi8randy@gmail.com>
2022-01-15 21:17:47 +0800

    added index map footer

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ function newsletter() {
     alert("Thank you for subscribing to our newsletter!");
   }
 }
+// Initialize and add the map
+function initMap() {
+  // The location of SIM
+  const SIM = { lat: 1.32941, lng: 103.77617 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 17,
+    mapId:"8895b6f658baf3db",
+    center: SIM,
 
+  });
+  // The marker, positioned at SIM
+  const marker = new google.maps.Marker({
+    position: SIM,
+    map: map,
+  });
+}
